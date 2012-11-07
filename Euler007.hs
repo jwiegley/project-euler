d7330054c17f77613e2b637bf871d0b044391a31
John Wiegley <johnw@newartisans.com>
2012-11-07 17:29:21 -0600

    Problem 7

---
+++ Euler007.hs
@@ -0,0 +1,27 @@
+module Euler007 where
+
+import Euler003
+
+import Control.Monad
+
+-- | Problem 7
+--
+-- By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+-- that the 6^th prime is 13.
+--
+-- What is the 10001^st prime number?
+--
+-- >>> euler007 6
+-- 13
+-- >>> euler007 10001
+-- 104743
+
+divisibleByAny :: Int -> Int -> Bool
+divisibleByAny n y = any (y `divisibleBy`) [3..lim n]
+  where lim = ceiling . sqrt . fromIntegral
+
+primes :: [Int]
+primes = 2 : filter (not . join divisibleByAny) [3,5..]
+
+euler007 :: Int -> Int
+euler007 n = primes !! (n - 1)
