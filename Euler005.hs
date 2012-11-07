072bab7a48322b49b895e5ad8bab244ada1db38b
John Wiegley <johnw@newartisans.com>
2012-11-07 14:39:20 -0600

    Removed some dead code

--- Euler005.hs
+++ Euler005.hs
@@ -20,23 +20,3 @@ divisibleByAll n y = all (y `divisibleBy`) [n,n-1..2]
 
 euler005 :: Int -> Int
 euler005 n = head $ filter (divisibleByAll n) [n,n+n..]
-
-------------------------------------------------------------------------
-
-multiples :: Integer -> [Integer]
-multiples n = iterate (+n) n
-
--- NOTE: This function assumes the input lists are sorted and infinite!
-lazyIntersect :: Ord a => [a] -> [a] -> [a]
-lazyIntersect xs ys =
-  let x     = head xs
-      cands = takeWhile (<= x) ys
-      rest  = lazyIntersect (tail xs) (dropWhile (<= x) ys)
-  in if not (null cands) && last cands == x
-     then x : rest
-     else rest
-
-euler005b :: Integer -> Integer
-euler005b n =
-  let xs = map multiples [2..n]
-  in head $ foldr lazyIntersect (head xs) (tail xs)
